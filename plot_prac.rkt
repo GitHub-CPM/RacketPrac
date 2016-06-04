25dafef77f9682fe76215efdf498aa8352b5f708
genxium <genxium@gmail.com>
2016-06-04 19:19:10 +0800

    Updated `plot_prac` for file exporting.

--- plot_prac.rkt
+++ plot_prac.rkt
@@ -9,7 +9,27 @@
                           (* (sin (* 0.5 t)) (sin (* 0.5 t)))
                          )
                  )))
-(define tmin 0.1)
-(define tmax (- (* 2 pi) 0.1))
-(define to-render (parametric f tmin tmax))
-(plot to-render)
+
+(define eps 0.01)
+(define tmin (- pi))
+(define t1 (- eps))
+(define t2 eps)
+(define t3 pi)
+(define t4 (- (* 2 pi) eps))
+(define t5 (+ (* 2 pi) eps))
+(define tmax (* 3 pi))
+
+(define to-render (list
+                   (parametric f tmin t1)
+                   (parametric f t2 t3)
+                   (parametric f t3 t4)
+                   (parametric f t5 tmax)
+                   ))
+
+(define wfilepath "./plot_prac_out.png")
+(plot-file to-render
+      wfilepath
+      'png
+      #:x-label "phi"
+      #:y-label "intensity"
+      #:title "Interference of N Emitters on Infinitely Distant Wall")
